b560596f6bfb5d4d2625219b2802575b9e6d77ca
Inqvizi <Firko06y@gmail.com>
2025-12-01 18:12:45 +0200

    Add Oleksandr Firko info and photo

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,10 +4,10 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+        <img src={"/oleksandrf.jpg"} alt="Oleksandr Firko" style={styles.photo} />
         <div style={styles.textContainer}>
-          <h2 style={styles.name}>John Doe</h2>
-          <p style={styles.position}>Software engineer</p>
+          <h2 style={styles.name}>Oleksandr Firko</h2>
+          <p style={styles.position}>Project Manager</p>
         </div>
       </div>
     </main>
